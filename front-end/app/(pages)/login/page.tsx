69f55142c90d6c028ed44df065c28a310ad5c040
Christine <christine.ocana@mlhuillier.com>
2024-06-29 08:05:47 +0800

    login updated

--- front-end/app/(pages)/login/page.tsx
+++ front-end/app/(pages)/login/page.tsx
@@ -1,26 +1,97 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Input from "@/app/components/Input/Input";
 import Button from "@/app/components/buttons/Button";
 
-const Login = () => {
+const Login: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {}
+  );
+
+  const validateEmail = (email: string) => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  };
+
+  const validatePassword = (password: string) => {
+    return password.length >= 6;
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    let validationErrors: { email?: string; password?: string } = {};
+    if (!email) {
+      validationErrors.email = "Email is required";
+    } else if (!validateEmail(email)) {
+      validationErrors.email = "Invalid email address";
+    }
+
+    if (!password) {
+      validationErrors.password = "Password is required";
+    } else if (!validatePassword(password)) {
+      validationErrors.password = "Password must be at least 6 characters long";
+    }
+
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      // Handle form submission logic here
+      console.log("Email:", email);
+      console.log("Password:", password);
+    }
+  };
+
   return (
-    <div className="container flex flex-col justify-center items-center p-10 gap-6">
-      <div>
-        <Image src="/images/logo.png" alt="logo" width={100} height={100} />
-      </div>
-      <div className="shadow-md rounded-lg border p-6 w-full flex flex-col items-center gap-6">
-        <h1 className="text-md font-bold">Login</h1>
-        <form action="" className="w-full">
-          <Input placeholder="Enter your email" name="email" />
-          <Input placeholder="Enter your password" name="password" />
-
-          <Button title="Submit" />
-
-          <a href="" className="text-sm text-blue-800 mb-4 hover:text-blue-700">
-            Forgotten Password?
-          </a>
-        </form>
+    <div className="container p-10">
+      <div className="flex flex-col justify-center items-center gap-6">
+        <div>
+          <Image src="/images/logo.png" alt="logo" width={100} height={100} />
+        </div>
+        <div className="shadow-md rounded-lg border p-6 w-full flex flex-col items-center gap-6">
+          <h1 className="text-md font-bold">Login</h1>
+          <form onSubmit={handleSubmit} className="w-full">
+            <div>
+              <Input
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Enter your email"
+                onChange={handleInputChange}
+                error={errors.email}
+              />
+            </div>
+            <div>
+              <Input
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Enter your password"
+                onChange={handleInputChange}
+                error={errors.password}
+              />
+            </div>
+            <Button title="Submit" />
+            <a
+              href="#"
+              className="text-sm text-blue-800 mb-4 hover:text-blue-700"
+            >
+              Forgotten Password?
+            </a>
+          </form>
+        </div>
       </div>
     </div>
   );
--- front-end/app/components/Input/Input.tsx
+++ front-end/app/components/Input/Input.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 interface InputProps {
   type: string;
@@ -6,23 +9,45 @@ interface InputProps {
   value: string;
   placeholder?: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
 }
+
 const Input: React.FC<InputProps> = ({
   type,
   name,
   value,
   placeholder,
   onChange,
+  error,
 }) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
   return (
-    <input
-      type={type}
-      name={name}
-      value={value}
-      placeholder={placeholder}
-      onChange={onChange}
-      className="p-4 text-sm border border-gray-200 rounded-md bg-gray-100 w-full mb-4"
-    />
+    <div className="relative w-full mb-4">
+      <input
+        type={isPasswordVisible && type === "password" ? "text" : type}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        className={`p-4 text-sm border ${
+          error ? "border-red-600" : "border-gray-200"
+        } rounded-md bg-gray-100 w-full`}
+      />
+      {type === "password" && (
+        <span
+          onClick={togglePasswordVisibility}
+          className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-400"
+        >
+          <FontAwesomeIcon icon={isPasswordVisible ? faEyeSlash : faEye} />
+        </span>
+      )}
+      {error && <p className="text-red-600 text-sm mt-1">{error}</p>}
+    </div>
   );
 };
 
